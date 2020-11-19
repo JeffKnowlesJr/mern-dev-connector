38e985be7b588c0b174be943cc879ecf32d45688
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-11-19 14:57:36 -0500

    Completed Section 4:  Profile API Routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 // Middleware function takes in req, res, next
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
   //Check if no token
-  if(!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied'});
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   //Verify token
@@ -16,7 +16,7 @@ module.exports = function(req, res, next) {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
     next();
-  } catch(err) {
-    return res.status(401).json({ msg: 'Token is not valid'});
+  } catch (err) {
+    return res.status(401).json({ msg: 'Token is not valid' });
   }
 };
