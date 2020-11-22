99677204a92e8c9070b8e2cd29faa9e616ba39ba
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-11-21 23:07:45 -0500

    Remote backup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
   // Use state hook
@@ -21,26 +21,7 @@ export const Register = () => {
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      };
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post('/api/users', body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log('SUCCESS');
     }
   };
 
@@ -100,7 +81,7 @@ export const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
