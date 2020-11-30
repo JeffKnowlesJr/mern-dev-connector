b394edaf91526e1141c5e6a2bec8bb0e1d04c16c
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-11-30 11:36:25 -0500

    Unknown changes - catch up commit

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+export default () => (
+  <Fragment>
+    <img
+      src={spinner}
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      alt="Loading..."
+    />
+  </Fragment>
+);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 // Include reducers
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 });
