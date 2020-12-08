8dd18844c7ae3885edd7ac05df7d4c13ddd15677
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-12-08 10:31:48 -0500

    Completed up through CreateProfile component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 
 // Load User
@@ -93,5 +94,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout / Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import spinner from './spinner.gif';
 
 export default () => (
   <Fragment>
