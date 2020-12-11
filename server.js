1e323ba4ab932064f9c4f14336c57797f5c60657
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-12-11 11:52:53 -0500

    updated server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,6 +20,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
