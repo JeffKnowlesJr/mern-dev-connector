a0d6311d376062cf14b86e045e8051fe6a1b073a
Jeff Knowles <jeffknowlesjr@gmail.com>
2020-12-11 11:29:23 -0500

    Readied for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,9 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
-
-app.get('/', (req, res) => res.send('API Running'));
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +16,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
