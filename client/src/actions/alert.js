249e301e048d75a01780988018eae7125e1ed35a
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-08-04 11:41:10 -0400

    rolled back change

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid'
 import { SET_ALERT, REMOVE_ALERT } from './types'
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
-  const id = uuidv4()
+  const id = uuid()
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
