34e410a28e9a77dde2ada4079f2bf4b6b5da82eb
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-08-04 11:37:59 -0400

    updated uuid call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,11 @@
-import { v4 as uuid } from 'uuid';
-import { SET_ALERT, REMOVE_ALERT } from './types';
+import { v4 as uuid } from 'uuid'
+import { SET_ALERT, REMOVE_ALERT } from './types'
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
-  const id = uuid();
+  const id = uuidv4()
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
-  });
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-};
+  })
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout)
+}
