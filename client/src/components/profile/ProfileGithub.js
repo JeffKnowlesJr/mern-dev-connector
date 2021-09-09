9867d4a1284e0ab9f5f50173c2582901f506732e
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-09-08 21:36:05 -0400

    Added link to repo and link validation

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
-import { getGithubRepos } from '../../actions/profile';
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
+import { getGithubRepos } from '../../actions/profile'
+
+const validateUrl = (value) => {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+    value
+  )
+}
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
-    getGithubRepos(username);
-  }, [getGithubRepos, username]);
+    getGithubRepos(username)
+  }, [getGithubRepos, username])
 
   return (
     <div className="profile-github">
@@ -28,6 +34,12 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
                 </a>
               </h4>
               <p>{repo.description}</p>
+              {validateUrl(repo.homepage) && (
+                <p>
+                  <h5>Url: </h5>
+                  <a href={repo.homepage}>{repo.homepage}</a>
+                </p>
+              )}
             </div>
             <div>
               <ul>
@@ -44,17 +56,17 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
         ))
       )}
     </div>
-  );
-};
+  )
+}
 
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
   repos: PropTypes.array.isRequired,
   username: PropTypes.string.isRequired
-};
+}
 
 const mapStateToProps = (state) => ({
   repos: state.profile.repos
-});
+})
 
-export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub);
+export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub)
